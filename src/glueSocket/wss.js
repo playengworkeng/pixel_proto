7794520a0a6e5d0e47a52fec145a37cd578f24ed
James Allen <jallen@awr8ja.scitec1.local>
2025-12-04 11:52:26 -0700

    updating fakerouter

--- src/glueSocket/wss.js
+++ src/glueSocket/wss.js
@@ -1,32 +1,59 @@
 const WebSocket = require('ws');
+const https = require('https');
+const fs = require('fs');
 
-const wss = new WebSocket.Server({port:8080});//new WebSocket.Server({port :8888})
-//const ws = new WebSocket("http://localhost:8080")
-// wss.on('connection', ws=>{
-//     console.log('client connected')
-// });
-console.log("I started up 8080");
-wss.onopen=()=>{
-    console.log("received data sniffer");
-    const sw = new WebSocket("ws://localhost:3000");
-    sw.on('message', sws=>{
-        console.log("connection made");
-        console.log(sws.toString('utf8'));
+const portLocal = 8080;
+let hws = null;
 
-        wss.send("Hello from sniffer");
+const options={
+    key: fs.readFileSync('./certs/key.pem'),
+    cert: fs.readFileSync('./certs/cert.pem')
+}
+
+let server = https.createServer( options, (req, res)=>{
+
+res.writeHead(200);
+
+})
+
+
+
+
+const wss = new WebSocket.Server({server});//new WebSocket.Server({port :8888})
+
+// wss.onopen=()=>{
+//     console.log("received data sniffer");
+//     const sw = new WebSocket("ws://localhost:3000");
+//     sw.on('message', sws=>{
+//         console.log("connection made");
+//         console.log(sws.toString('utf8'));
+
+//         wss.send("Hello from sniffer");
      
-    
-      }   ) 
-    }
+//       }   ) 
+//     }
 
 
-wss.on('message',data=>{console.log(data.toString('utf8'))})
+// wss.on('message',data=>{console.log(data.toString('utf8'))})
 
 wss.on('error',error=>{
     console.error(error)
 })
-// wss.on('connection',(event)=>{
-//         console.log("websocket established")
-//     });
+
+wss.on('connection',(ws)=>{
+        console.log("websocket established");
+        ws.on('error', console.error);
+        ws.send('hello')
+    });
 
 
+//add the upgrade logic when coming from http
+server.on('upgrade', (request, socket, head)=>{
+    wss.handleUpgrade(request, socket, head, (ws)=>
+    {    ws.send('hello')
+        wss.emit('connection', ws, request);
+    })
+
+});
+
+server.listen( portLocal, ()=> console.log('running https on port 8080'));
