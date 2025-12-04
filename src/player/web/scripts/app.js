8211c4e3f727e6345ce399278d1468ad4dead1a2
James Allen <jallen@awr8ja.scitec1.local>
2025-12-04 14:04:02 -0700

    updating fakerouter

--- src/player/web/scripts/app.js
+++ src/player/web/scripts/app.js
@@ -1881,7 +1881,7 @@ function connect() {
       '/ws' + window.location.search; // websocket route
 
   //temporary for now just to get a connection, may employ sidecar later
-  wsUrl = "ws://pixel-proto-gluesocket:8080"
+  wsUrl = "http://172.30.250.253:8080";//"ws://pixel-proto-gluesocket:8080"
   ws = new WebSocket(wsUrl);
 
   console.log(wsUrl);
