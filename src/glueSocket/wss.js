bb995a8977a4e3a3d44f08737fc2daf70444d284
James Allen <jallen@scitec.com>
2025-12-03 22:33:49 -0700

    updating fakerouter

--- src/glueSocket/wss.js
+++ src/glueSocket/wss.js
@@ -1,28 +1,28 @@
 const WebSocket = require('ws');
 
-// const wss = new WebSocket.Server({port:8888});//new WebSocket.Server({port :8888})
-const ws = new WebSocket("http://localhost:8080")
+const wss = new WebSocket.Server({port:8088});//new WebSocket.Server({port :8888})
+//const ws = new WebSocket("http://localhost:8080")
 // wss.on('connection', ws=>{
 //     console.log('client connected')
 // });
 console.log("I started up");
-ws.onopen=()=>{
+wss.onopen=()=>{
     console.log("received data sniffer");
     const sw = new WebSocket("ws://localhost:3000");
     sw.on('message', sws=>{
         console.log("connection made");
         console.log(sws.toString('utf8'));
 
-        ws.send("Hello from sniffer");
+        wss.send("Hello from sniffer");
      
     
       }   ) 
     }
 
 
-ws.on('message',data=>{console.log(data.toString('utf8'))})
+wss.on('message',data=>{console.log(data.toString('utf8'))})
 
-ws.on('error',error=>{
+wss.on('error',error=>{
     console.error(error)
 })
 // wss.on('connection',(event)=>{
