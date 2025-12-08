c2f7fc6ce6e142831466340dc3948a7a03ef3d19
James Allen <jallen@awr8ja.scitec1.local>
2025-12-08 15:45:34 -0700

    updatding fakeunreal

---
+++ src/fake_unreal_app/unreal.js
@@ -0,0 +1,73 @@
+const WebSocket = require('ws')
+const connectionURL = process.env.CIRRUS_SERVER;//"";
+// const RTCPeerConnection = require('@roamhq/wrtc').RTCPeerConnection;
+
+let server = null;
+// let iPeerConnection = RTCPeerConnection({
+//     sdpSemantics: 'unified-plan'
+// });
+
+async function createAnswer(ws){
+
+    // const firstAnswer = iPeerConnection.createAnswer();
+    const updatedAnswer ={
+        type: 'answer',
+        sdp: "None"
+    };
+
+
+    console.log("sending sdp");
+
+    ws.send(JSON.stringify( updatedAnswer));
+
+}
+
+function connect(){
+
+    try{
+    if ( connectionURL )
+    {
+        server = new WebSocket(connectionURL)
+        server.on('connection', (ws)=>{
+            console.log("we have connection");
+
+            ws.on('message', (data)=>{
+
+                msg = JSON.parse(data);
+
+                if (msg.type =='offer')
+                {
+                    console.log(msg);
+                    console.log("Create an answer for offer");
+                    createAnswer(ws)
+                }
+            })
+        })
+    }
+    }catch (error)
+    {
+        console.log(error);
+        exit();
+    }
+}
+
+function main()
+{
+    try{
+        connect();
+        console.log(`getting ready to connect to ${connectionURL}`);
+    }
+    catch(error){
+
+        console.log(error.message);
+        exit();
+    }
+    
+}
+
+process.on('uncaughtException', function(err) {
+      // handle the error safely
+      console.log(err)
+  })
+
+main();
