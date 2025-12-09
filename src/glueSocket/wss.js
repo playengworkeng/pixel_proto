e73e00d2434562d68d8b4fa89c9acccdc8c930e0
James Allen <jallen@awr8ja.scitec1.local>
2025-12-09 10:29:13 -0700

    updating fakerouter

--- src/glueSocket/wss.js
+++ src/glueSocket/wss.js
@@ -54,7 +54,7 @@ function routeToCirrus(ws)
 }
 
 
-const wss = new WebSocket.Server({server, path:'/'});//new WebSocket.Server({port :8888})
+const wss = new WebSocket.Server({noServer:true, path:'/'});//new WebSocket.Server({port :8888})
 
 wss.on('connection',(ws)=>{
         console.log("websocket established");
