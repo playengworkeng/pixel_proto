f123aa22e7c5b55dd8c5fb19ce6e00405ed72565
James Allen <jallen@awr8ja.scitec1.local>
2025-12-10 15:50:20 -0700

    update fakerouter

--- src/glueSocket/wss.js
+++ src/glueSocket/wss.js
@@ -76,7 +76,8 @@ const wss = new WebSocket.Server({ noServer: true, path: "/" }); //new WebSocket
 
 wss.on("connection", (ws, req) => {
   console.log(`websocket established from ${req.url}`);
-  console.log(`total websocket established from ${JSON.stringify(req)}`);
+  console.log(`total websocket established from ${req.socket.remoteAddress}`);
+   console.log(`total websocket established from ${req.socket.localAddress}`);
   ws.on("error", console.error);
   ws.on("message", (data) => {
     console.log(data.toString("utf8"));
