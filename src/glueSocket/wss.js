4f5ddd0459e121d6522b214a6c3c2ef43b15c10b
James Allen <jallen@scitec.com>
2025-12-10 02:05:56 -0700

    updating fakerouter

--- src/glueSocket/wss.js
+++ src/glueSocket/wss.js
@@ -49,11 +49,11 @@ function routeToCirrus(ws,inMessage)
             if ( msg.type == 'answer' || msg.type =='config')
             {
 
-                console.log(`sending ${ms} to player`)
-            ws.send(ms);
+                console.log(`sending ${mmessages} to player`)
+            ws.send(message);
             }
 
-           ws.send(ms);
+           ws.send(message);
         })
 
         wc.send(inMessage);
